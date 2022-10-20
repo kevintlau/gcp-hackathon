a90cc66c9ac6e7b9289c1d02916ed6d29dd3bcce
Kevin Lau <klau@boingo.com>
2022-10-20 11:57:39 -0700

    updates movement

--- web.js
+++ web.js
@@ -10,11 +10,43 @@ app.get('/', function (req, res) {
 
 app.post('/', function (req, res) {
   console.log(req.body);
-  const moves = ['F', 'T', 'L', 'R'];
+  // const moves = ['F', 'T', 'L', 'R'];
+  const moves = ['T', 'L', 'T', 'R'];
+
+  const selfUrl = req.body[_links].self.href;
   
-  // TODO add your implementation here to replace the random response
+  const {
+    arena: {
+      state: {
+        selfUrl: {
+          x,
+          y,
+          direction,
+        }
+      }
+    }
+  } = req.body;
+
+  if (x !== 3) {
+    if (x < 3) {
+      if (direction !== "E") res.send("R");
+      else res.send("F");
+    } else if (x > 3) {
+      if (direction !== "W") res.send("R");
+      else res.send("F");
+    }
+  } else if (y !== 3) {
+    if (y < 3) {
+      if (direction !== "S") res.send("R");
+      else res.send("F");
+    } else if (y > 3) {
+      if (direction !== "N") res.send("R");
+      else res.send("F");
+    }
+  } else {
+    res.send(moves[Math.floor(Math.random() * moves.length)]);
+  }
   
-  res.send(moves[Math.floor(Math.random() * moves.length)]);
 });
 
 app.listen(process.env.PORT || 8080);
